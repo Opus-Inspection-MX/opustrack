4ab2ae837f28f87e191b4a91ae15cd7c27dca221
Abdiel Reyes <abykings1@hotmail.com>
2025-08-05 02:16:09 -0600

    fix: fixed dockerfiles to add .env files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const devOrigin = process.env.DEV_ORIGIN || 'http://localhost:3000';
+  const prodOrigin =
+    process.env.PROD_ORIGIN || 'https://opustrack.abdielreyes.com';
 
   app.enableCors({
-    origin: 'http://localhost:3000', // tu front‑end
+    origin: process.env.NODE_ENV === 'production' ? prodOrigin : devOrigin, // tu front‑end
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // si envías cookies / auth
     allowedHeaders: 'Content-Type,Authorization',
   });
   app.setGlobalPrefix('api'); // opcional, si quieres un prefijo global
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(8000);
 }
 bootstrap();
